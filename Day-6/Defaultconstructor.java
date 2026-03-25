e9a33c3db05c23617ba64f8880e0e026ee928db2
Sangeetha Rajendren <rajendrensangeetha2006@gmail.com>
2026-03-25 14:08:50 +0530

    Update and rename Defaultconstructor.java to Day-6/Defaultconstructor.java

--- Day-6/Defaultconstructor.java
+++ Day-6/Defaultconstructor.java
@@ -3,7 +3,7 @@ public class Defaultconstructor {
         System.out.println("I am inside default constructor");
     }
     public static void main(String[] args) {
-        Defaultconstructor obj1 = new Defaultconstructor();
-        System.out.println("See your ");
+        
+        System.out.println("See your class automatically creates the default constructor  ");
     }
 }
