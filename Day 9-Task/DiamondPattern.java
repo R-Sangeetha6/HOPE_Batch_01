0027a8a7d61bde16b8454c4058a163b5eacd8579
Sangeetha Rajendren <rajendrensangeetha2006@gmail.com>
2026-03-30 21:52:34 +0530

    Update and rename Day 9-Task.java to Day 9-Task/DiamondPattern.java

--- Day 9-Task/DiamondPattern.java
+++ Day 9-Task/DiamondPattern.java
@@ -3,15 +3,24 @@ interface Person {
 }
 
 interface Student extends Person {
+    void  study();
 }
 
 interface Teacher extends Person {
+    void teach();
 }
 
 class CollegeMember implements Student, Teacher {
 
     public void role() {
         System.out.println("Person can be both Student and Teacher in the college");
+    }
+    public void study()
+    {
+        System.out.println("Student studies subject");
+    }
+    public void teach(){
+        System.out.println("Teacher teaches subject");
     }
 }
 
@@ -20,6 +29,8 @@ public class Main {
 
         CollegeMember c = new CollegeMember();
         c.role();
+        c.study();
+        c.teach();
 
     }
 }
